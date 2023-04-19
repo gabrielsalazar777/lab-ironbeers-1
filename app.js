ca5cc25bbf29192edf945e3379b0f21d9c5c74a1
Gabriel Salazar <gabriel.salazar777@gmail.com>
2023-04-19 18:16:41 -0400

    assignment complete minus bonus 7

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+punkAPI;
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -14,9 +16,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { beers: beersFromApi }))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+      res.render('random-beers', { beers: beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/beer/:beerid', (req, res) => {
+  punkAPI
+    .getBeer(req.params.beerid)
+    .then(responseFromAPI => {
+      res.render('random-beers', { beers: responseFromAPI });
+    })
+    .catch(error => console.log(error));
+});
 
 app.get('/', (req, res) => {
   res.render('index');
